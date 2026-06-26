15d24eb8d7c76b175d7f26c2fa437bc84e910453
akryaz <akryazbim@gmail.com>
2026-06-26 20:07:32 +0530

    Initial commit

---
+++ Python Samples/Demo.py
@@ -0,0 +1,820 @@
+
+#ctrl + /
+
+# fhsfhsfh567890-=$%^&*() nfgbg,filePath = ""
+# djgbjasd
+# gnalsdngla
+# dgng
+# fghsf
+
+# fp = r"C:\Users\Akryazz\OneDrive\Documents\My Files\new file" #My Comment
+
+
+# x = 10
+# y = float(x)
+
+# a = 10.597986465123
+# b = int(a)
+
+# m = 50.5
+# n = str(m)
+
+# q = "78.9"
+# e = int(float(q))
+
+
+# print(e)
+
+
+
+# x = 10 <= 5
+
+# xyz = "Hey there  "
+
+# print(10 in 100)
+
+
+
+
+
+# x = 2
+
+# if x>5:  
+#     print("It  is greater than 5")
+#     print("Yes")
+# else:
+#     print("It is not greater than 5")
+#     print("No")
+
+# print("END")
+
+
+# xy= 21
+# if ((xy%2) == 0):
+#     print("It's even number")
+# else:
+#     print("Its odd number")
+# print("The program is over")
+
+
+# x = 8 
+# if  x%2 != 0:
+#     print("it is a odd number")
+#     print("True")
+# else:
+#     print("it is even number")
+#     print("False")
+# print("END")
+
+
+# x = input("Enter a number: ")
+# x = int(x)
+# operation = x/2
+# value = str(operation)
+
+# #10/2 = 5.0 -> 0 => even
+# #15/2 = 7.5 -> 5 => odd
+
+# lastItem = value[-1]
+
+# if lastItem == "0":
+#     print("It is an even number")
+# else:
+#     print("It is an odd number")
+
+
+
+
+
+
+
+
+# x = 100
+# y = 1000
+
+# if x>y:
+#     print("x is greater than y")
+# elif x==y:
+#     print("x is equal to y")
+# elif y>x:
+#     print("y is greater than x")
+
+
+
+#print((10>5 and (5>30 or 74>45)) or 98<45)
+
+
+
+# a = 7
+# b = 6
+# c = 10
+
+# if a>b and a>c:
+#     print("a is the greatest number")
+# elif b>a and b>c:
+#     print("b is the greatest number")
+# else:
+#     print("c is the greatest number")
+
+
+
+
+# x = 10
+# y = 10
+
+# if x>y:
+#     print("x is greater than y")
+#     if x%2 == 0:
+#         print("x is even number")
+#         if x>100:
+#             print("x is greater than 100")
+#             if x%10 == 0:
+#                 print("x is multiple of 10")
+#             else:
+#                 print("x is not multiple of 10")
+#         else:
+#             print("x is less than 100")
+#     else:
+#         print("x is odd number")
+#     print("if block end")
+# elif x==y:
+#     print("x is equal to y")
+#     if x%2 == 0:
+#         print("x is even number")
+#     else:
+#         print("x is odd number")
+#     print("elif block end")
+# else:
+#     print("y is greater than x")
+#     if y%2 == 0:
+#         print("y is even number")
+#     else:
+#         print("y is odd number")
+#     print("elif block end") 
+
+# print("END")
+
+
+
+# xyz = "Hey there"
+
+# if "Hey" not in xyz:
+#     print("No, 'Hey' is not present in the string")
+# else:
+#     print("Yes, 'Hey' is present in the string")
+
+
+# print(15//2)
+
+# a = 10
+# b = 20
+# c = 30
+
+# if a>b and a>c and b>c:
+#     print("a is largest and c is lowest")
+# elif a>b and a>c and c>b:
+#     print("a is largest and b is lowest")
+# elif b>a and b>c and c>a:
+#     print("b is largest and a is lowest")
+# elif b>a and b>c and a>c:
+#     print("b is largest and c is lowest")
+# elif c>a and c>b and a>b:
+#     print("c is largest and b is lowest")
+# elif c>a and c>b and b>a:
+#     print("c is largest and a is lowest")
+# elif a==b and b==c:
+#     print("all are equal")
+# elif a==b and b!=c:
+#     print("a is equal to b, but b is not equal to c")
+# elif b==c and c!=a:
+#     print("b is equal to c, but c is not equal to a")
+# elif a==c and b!=a:
+#     print("a is equal to c, but b is not equal to a")
+# elif a%2 == 0:
+#     print("a is even no.")
+# else:
+#     print("a is odd no.")
+
+# elif b%2 == 0:
+#     print("b is even no.")
+# else:
+#     print("b is odd no.")
+
+# elif c%2 == 0:
+#     print("c is even no.")
+# else:
+#     print("c is odd no.")
+
+
+# a = 33
+# b = 52
+# c = 98
+
+# if a>b and a>c:
+#     print("a is greater than b and c")
+#     if a%2 == 0:
+#         print ("a is even number")
+#     else: 
+#         print("a is odd number")
+# elif b>a and b>c:
+#    print("b is greater than a and c")
+#    if b%2 ==0:
+#       print("b is odd number")
+#    else:
+#       print("b is a even number")
+     
+# elif a==b==c:
+ 
+#  print("a is equal to b and c")
+#  if a%2 == 0:
+#     print("a is even number")
+#  else:
+#     print ("a is an odd numbe")
+# print ("END")
+
+
+# a = 18  
+# b = 16
+# c = 21
+# if a>b and (b>c or a>c or b==c) :
+#     print ("a is gretest")
+# elif b>c and (b>a or c>a or a==c) :
+#     print ("b is greatest")
+# elif (a==b and b==c) :
+#     print ("All are equal")
+# else :
+#     print ("c is geatest")
+# print ('end program')
+
+
+# a= 20
+# b = 40
+# c = 60
+# if a > b and a > c:
+#   print("a is greater")
+#   if a > 100:
+#     print("a is greater than 100")
+#   else:
+#     print("a is not greater than 100")
+#   if a%2 == 0:
+#     print("a is even")
+#   else:
+#     print("a is odd")
+  
+# elif b > a and b > c:
+#   print("b is greater")
+#   if b > 100:
+#     print("b is greater than 100")
+#   else:
+#     print("b is not greater than 100")
+#   if b%2 == 0:
+#     print("b is even")
+#   else:
+#     print("b is odd")
+# elif c > a and c > b:
+#   print("c is greater")
+#   if c > 100:
+#     print("c is greater than 100")
+#   else:
+#     print("c is not greater than 100")
+#   if c%2 == 0:
+#     print("c is even")
+#   else:
+#     print("c is odd")
+# elif a == b and a == c:
+#   print("a,b,c are equal")
+#   if a > 100:
+#     print("a,b,c is greater than 100")
+#   else:
+#     print("a,b,c is not greater than 100")
+#   if a%2 == 0:
+#     print("a,b,c are even")
+#   else:
+#     print("a,b,c are odd")
+  
+# elif a == b:
+#   print("a and b are equal")
+#   if a > 100:
+#     print("a and b are greater than 100")
+#   else:
+#     print("a and b are not greater than 100")
+#   if a%2 == 0:
+#     print("a and b are even")
+#   else:
+#     print("a and b are odd")
+# elif a == c:
+#   print("a and c are equal")
+#   if a > 100:
+#     print("a and c are greater than 100")
+#   else:
+#     print("a and c are not greater than 100")
+#   if a%2 == 0:
+#     print("a and c are even")
+#   else:
+#     print("a and c are odd")
+# else:
+#   print("b and c are equal")
+#   if b > 100:
+#     print("b and c are greater than 100")
+#   else:
+#     print("b and c are not greater than 100")
+#   if b%2 == 0:
+#     print("b and c are even")
+#   else:
+#     print("b and c are odd")
+# print (END)
+
+
+
+# a = 10
+# b = 20
+# c = 12
+
+# greatest = 0
+
+# if a>b and a>c:
+#     print("a is greater than b and c")
+#     greatest = a
+# elif b>a and b>c:
+#     print("b is greater than a and c")
+#     greatest = b
+# elif c>a and c>b:
+#     print("c is greater than a and b")
+#     greatest = c
+
+# if greatest%2 == 0:
+#     print("The greatest number is even")
+# else:
+#     print("The greatest number is odd")
+
+# a = 100
+# b = 200
+# c = a/b
+
+# if a>b:
+#     print("a is greater than b")
+#     if type(a) == int:
+#             print("a is an integer")
+#     else:
+#             print("a is not an integer")
+# elif b>a:
+#     print("b is greater than a")
+#     if type(b) == int:
+#             print("b is an integer")
+#     else:
+#             print("b is not an integer")
+
+
+# if c>1 and type(c) == float:
+#     print("c is a decimal number greater than 1", c)
+# else:
+#     print("c is not a decimal number greater than 1", c)
+
+
+
+
+
+
+
+
+
+#myList = [1, 2, 6.5, "abc", True, 10+20, 5>2 and 10<50, [10, 20,30,[100,200,300],40]]
+
+
+# print(len(myList))
+
+
+# i = myList[-1]
+
+# print(i)
+
+# j = i[3]
+
+# print(j)
+
+# k = j[-1]
+
+# print(k)
+
+
+#print(myList[-1][3][-1])
+
+
+#[1..100..2]
+#print(list(range(1,101,2)))
+
+
+
+# numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+# xyz = [11, 12, 13, 14, 15]
+
+
+# print(numbers)
+
+# numbers.append("11")
+# numbers.append([10,20])
+
+# print(numbers)
+
+# print(numbers)
+
+# numbers[1] = 2.5
+
+# print(numbers)
+
+
+# numbers = [100, 200, 300, 400, 500]
+# numbers[5] = 250
+# #numbers.append(2.5)
+# numbers.insert(-150, 250)
+
+
+
+
+# i = 1
+# while (i <= 10):
+#     if i%2 == 0:
+#         print(i)
+#     i += 1
+
+# print("END")
+
+
+# num = 2
+# while num <= 100:
+#     is_prime = True
+#     divisor = 2
+#     while divisor < num:
+#         if num % divisor == 0:
+#             is_prime = False
+#             break
+#         divisor += 1
+#     if is_prime:
+#         print(num)
+#     num += 1
+
+
+
+# sum = 0
+
+# i = 1
+# while i<=100:
+#     sum = sum + i
+#     i = i+1
+
+# print("The sum is:", sum)   
+
+
+# i = 10
+# while i>=1:
+#     print(i)
+#     i = i-1
+
+# j = 0
+# i = 1
+# while i<=100:
+#     j = j+i
+#     i=i+1 #OR i += 1
+
+# print(j)
+
+
+
+# fruits = ["apple", "banana", "cherry", "date", [10,20,30], "mango"]
+
+# i = 0
+# while i <= (len(fruits)-1):
+#     print(fruits[i])
+#     i += 1
+
+
+# for abc in fruits:  
+#     print(abc)
+
+
+
+# for i in range(1,11) : print (i)
+
+# sum = 0
+# for i in range(1,11):
+#     sum = sum+i
+#     print(i)
+# print(sum)
+
+
+# sum = 0
+# for i in range(1,11):
+#     sum = sum + i
+# print (sum)
+
+# for i in range(2, 11, 2):
+#      print(i)
+
+# for i in range(1,51) : print (2*i)
+
+# sum = 0
+# for i in range(1,101) :
+#     if (i%2) == 0 :
+#         sum = sum + i
+# print (sum)
+
+# sum=0
+# for i in range(2,101,2):
+#     sum=sum+i
+    
+# print("Sum of even numbers from 1 to 100 is:", sum)
+
+# import copy
+# # expected results
+# numbers = [1,2,3,4,5]
+# numbers1 = numbers.copy()
+# numbers1.append(99)
+# print(numbers)
+# print(numbers1)
+# not as expected
+# numbers = [[1,2],3,4,5]
+# numbers1 = copy.deepcopy(numbers)
+# numbers1[0].append(99)
+# print(numbers)
+# print(numbers1)
+
+
+# sumEven = 0
+# sumOdd = 0
+# for i in range(1,100):
+#     if i%2 == 0:
+#         sumEven = sumEven+i
+#     else:
+#         sumOdd = sumOdd+i
+
+# print("The sum of Even number is ", sumEven)
+# print("The sum of Odd number is ", sumOdd)
+
+
+
+
+fruits = ["apple", "banana", "cherry", "date", "mango"]
+
+# for i in fruits:
+#     if i == "banana":
+#         print("I got my banana")
+#         break
+#     print("hey")
+#     print("hello")
+
+# for i in fruits:
+#     if i == "banana":
+#         continue
+#     print("I got",  i)
+
+# print("END")
+
+
+# xyz = 25
+# print(f"The value is {xyz}.END")
+
+
+# print("START")
+
+# def greet():
+#     print("Good evening everybody!!!!")
+#     print("How are you?")
+#     print("Nice to see you")
+
+# def sum():
+#     a = 10
+#     b = 15
+#     print(a+b)
+#     return a+b
+
+# xyz = sum()
+# print(xyz)
+
+# a = 100
+# b = 150
+# # sum()
+
+# # print(a)
+# # print(b)
+
+
+# def add(x, y):
+#     z = x+y
+#     return z
+
+
+# result = add(10,20)
+# print(result)
+
+
+# print("END")
+
+# def sum(i,j):
+#     Total = i+j
+#     print("The total number is", Total)
+
+# def sub(i,j):
+#     Total = i-j
+#     print("The subtracted number is", Total)
+    
+# def div(i,j):
+#     Total = i/j
+#     print("The Quotient is", Total)
+# def mlt(i,j):
+#     Total = i*j
+#     print("The multiplied number is", Total)
+
+# cal=["+", "-","/","X","x","*"]
+# while True:
+#     a = str(input("Please enter Which Operation you want to perform (+ - / X) : "))
+#     if a in cal:
+#         i = float(input("Enter the first Number : "))
+#         j = float(input("Enter the second Number : "))
+#         if a == "+":
+#             result = sum(i,j)
+#             print(result)
+#         elif a == "-":
+#             sub(i,j)
+#         elif a == "/":
+#             div(i,j)
+#         else:
+#             mlt(i,j)
+#         print("Yes! you got your answer")
+#         choice = input("Do you wnat to perform another operation (Y/N):")
+#         if choice == "N": break
+#     else:
+#         print("Boom ! Enter correct symbol ")
+    
+
+# numbers = list(range(3,10))
+# for number in numbers:
+#     for i in range(2,number):
+#         if number%i == 0:
+#             print(f"{number} is composite number")
+#             break
+#     else:
+#         print(f"{number} is prime number")
+
+
+# a = [1, True, 0, False, 1, True, "apple", 1, 1]
+
+# sum = 0
+
+# value = 1
+
+# for x in a:
+#     if type(x) == int and x == value:
+#         sum += 1
+
+# print(sum)
+
+
+# print("Calculator\n")
+# try:
+#     num1 = float(input("Enter first number: "))
+#     num2 = float(input("Enter second number: "))
+#     print("1.Add \n2.Subtract \n3.Multiply \n4.Divide")
+#     choice = input("Enter choice: ")
+#     if choice == "1":
+#         print("Result:", num1 + num2)
+#     elif choice == "2":
+#         print("Result:", num1 - num2)
+#     elif choice == "3":
+#         print("Result:", num1 * num2)
+#     elif choice == "4":
+#         if num2 != 0:
+#             print("Result:", num1 / num2)
+#         else:
+#             print("Cannot divide by zero")
+#     else:
+#         print("Invalid choice")
+# except:
+#     pass
+    #print("Please enter valid numbers")
+
+
+
+
+
+
+
+
+
+
+
+
+# class Student:
+#     Name = "XYZ"
+#     RollNumber = 0
+#     Department = "BIM"
+#     Gender = "XYZ"
+
+#     def Study(self):
+#         print(f"{self.Name} is studying....")
+
+#     def WriteExam(self):
+#         print(f"{self.Name} is writing exam....")
+
+# s1 = Student()
+# s1.Name = "Ashim"
+
+# s2 = Student()
+# s2.Name = "Adithya"
+# print(Student().Name)
+
+
+
+# class CableTray:
+#     Type = "XYZ"
+#     Material = "ABC"
+#     ServiceType = "PQR"
+#     Width = 100
+#     def Move(self):
+#         print(f"{self.ServiceType} CT is moving from A to B")
+
+#     def width(self):
+#         print(f"The width of the CT is {self.Width}")
+
+#     def type(self):
+#         print(f"{self.Type} CT type changing from Perforated to Ladder ")
+
+# ct1 = CableTray()
+# ct1.ServiceType = "Medium Volt"
+# ct1.Type = "EMT"
+# ct1.Material = "Iron"
+# #print(ct1.ServiceType)
+
+# ct2 = CableTray()
+# ct2.ServiceType = "Low Voltage"
+# ct2.Type = "RMC"
+# ct2.Material = "Still"
+# #print(ct2.ServiceType)
+
+# ct1.Move()
+#ct2.width()
+# ct1.type()
+# ct2.type()
+# ct2.Move()
+
+
+# class RevitFamily:
+#     level="Level 1"
+#     material="Concrete"
+#     color = "Grey"
+#     length = 10
+#     width = 5
+#     def ComputeArea(self):
+#         print(f"The area is: {self.length * self.width}")
+#     def info(self):
+#         print(f"Level: {self.level}, Material: {self.material}, Color: {self.color}, Length: {self.length}, Width: {self.width}")  
+# R1=RevitFamily()
+ 
+# R1.ComputeArea()
+# R1.info()
+
+
+# class wall:
+#     wall_thk = 300
+#     wall_name = "xyz"
+#     wall_length = 300
+#     wall_base = "Level 1"
+#     wall_top = "Level 2"
+#     wall_ht = 300
+#     def CreateWall(self):
+#         print(f"{self.wall_name} of {self.wall_thk}mm thick and {self.wall_length}mm length and {self.wall_ht}mm height is created from {self.wall_base} to {self.wall_top}")  
+# w1=wall()
+# w1.wall_thk = 230
+# w1.wall_name = "Main Wall"
+# w1.wall_length = 3000
+# w1.wall_ht = 3000
+# w1.wall_top = "Level 1"
+# w1.wall_base = "Level 0"
+# w1.CreateWall()
+
+
+# def Tables(a, b, c):
+#     sum = 0
+#     for i in range (a,b):
+#         i=(i*c)
+#         print(i)
+#         sum = sum + i
+#     return sum
+        
+# sumOfAllNumbers = Tables(1,6,2)
+# print("The sum of all numners:",  sumOfAllNumbers)
+# print("END")
+
+
+
+
+def Add(a,b):
+    print(a+b)
+
+def Sub(x,y):
+    print(x-y)
+
+def Greet():
+    print("Hey, How you doing?")
+
+
+
---
+++ Python Samples/LibrariesDemo.py
@@ -0,0 +1,16 @@
+
+
+# import sys
+# sys.path.append(r"C:\Users\Akryazz\repos\RevitAPI\June 2026\Python Samples")
+
+
+#import Demo
+#import Demo as D
+#from Demo import Add, Sub
+from Demo import *
+
+Add(41,52)
+Sub(10,5)
+
+Greet()
+
